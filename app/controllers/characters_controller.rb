d75be7bee88c88d2d0acfbdde9539ea56a52987a
Zach Hofley <zhofley@gmail.com>
2018-03-06 12:04:22 -0500

    actually added to controller

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,3 +1,5 @@
+require 'json'
+
 class CharactersController < ApplicationController
   before_action :set_character, only: [:show, :destroy]
 
@@ -6,18 +8,51 @@ class CharactersController < ApplicationController
   end
 
   def create
-    @character = Character.new(character_params)
 
-    if current_user
-      @character.user = current_user
-      if @character.save
-        notice: "Succes, character save"
-      else
-        notice: "Something went wrong, please try again"
-      end
-    else
-      redirect_to new_user_session
+    def load_attributes(file_name)
+      file = File.read(file_name)
+      text = JSON.parse(file)
+
+      @attributes = {
+        templates: text["templates"],
+        names: text["names"],
+        traits: text["traits"],
+        races: text["races"],
+        jobs: text["jobs"],
+        locations: text["locations"],
+        quirks: text["quirks"]
+      }
     end
+
+    def generate
+      attributes = load_attributes('text.json')
+      template = attributes[:templates].sample
+
+      @character_params = {
+        'name' => attributes[:names].sample,
+        'trait' => attributes[:traits].sample,
+        'race' => attributes[:races].sample,
+        'job' => attributes[:jobs].sample,
+        'location' => attributes[:locations].sample,
+        'quirk' => attributes[:quirks].sample
+       }
+
+      template.gsub(/trait|race|job|location|quirk/) { |match| @character_params[match] }
+    end
+
+    @sentence = generate
+    @character = Character.new(@character_params)
+    @character.save
+    # if current_user
+    #   @character.user = current_user
+    #   if @character.save
+    #     notice: "Succes, character save"
+    #   else
+    #     notice: "Something went wrong, please try again"
+    #   end
+    # else
+    #   redirect_to new_user_session
+    # end
   end
 
   def show
@@ -40,7 +75,4 @@ class CharactersController < ApplicationController
     @character = Character.find(params[:id])
   end
 
-  def character_params
-    params.require(:character).permit(:name, :race, :class, :location)
-  end
 end
