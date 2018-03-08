9cb03ce7922a2a6389ee50f3d1f1c58f26d77680
Zach Hofley <zhofley@gmail.com>
2018-03-08 14:18:56 -0500

    Added full generator in services

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :characters, except: [:edit, :update]
-  resources :generations, only: [:new]
+  resources :generations, only: [:new, :build]
 end
