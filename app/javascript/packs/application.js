dbca023490bd47fd07c31744f8b794128914fd80
Aayush Patel <patelaa77@gmail.com>
2018-03-13 14:52:23 -0400

    footer-edit-text

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,4 +1,6 @@
 import "bootstrap";
 import { initializeTypedJs } from "./../components/banner.js"
+import { initializeTypedJs } from "./../components/new.js"
 
+initializeTypedJs();
 initializeTypedJs();
