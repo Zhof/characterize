07534b3a387d109cb22c37b9ef05f7d4ef2656e9
Aayush Patel <patelaa77@gmail.com>
2018-03-14 11:10:49 -0400

    footer-alignment

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,9 +1,10 @@
 import "bootstrap";
 
 import { initializeTypedJs } from "./../components/banner.js"
-import { initializeTypedJs } from "./../components/new.js"
+// import { initializeTypedJsNewCharacter } from "./../components/new.js"
 
 import { initializeCharacterForm } from "./../components/character_form.js";
 
 initializeTypedJs();
+// initializeTypedJsNewCharacter();
 initializeCharacterForm();
